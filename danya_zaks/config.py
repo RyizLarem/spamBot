d1e26b81a3008a7d427da3c0e0daa7e8b1c82189
ryiz <ryizaks@gmail.com>
2022-01-15 19:19:08 +0300

    spam bot 0.1

---
+++ danya_zaks/config.py
@@ -0,0 +1,24 @@
+import random
+
+delete_invalid_tokens = True
+
+tokens_file = 'tokens.txt'
+id_file = 'test.txt'
+
+rucaptcha_api_key = "ваш ключи от rucaptcha"
+
+
+message = {
+    'message': """text""",
+    'attachment': ''
+}
+
+
+
+
+
+
+
+
+
+
